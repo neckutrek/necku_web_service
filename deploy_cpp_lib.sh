6baf269fdfaa528b989bc5d6d6aea65e17037f48
neckutrek <>
2022-05-26 21:24:07 +0200

    clean up idea projects

--- deploy_cpp_lib.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cp ./cpp_lib/cmake-build-release/libnative.so ./java_web_service/lib
--- run_app.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Djava.library.path=".:/home/neckutrek/git/necku_web_service/java_web_service/lib" -jar java_web_service/target/demo-0.0.1-SNAPSHOT.jar
