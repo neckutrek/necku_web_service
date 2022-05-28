19f4c190b55c2240ef9d1a41fae73f5bc70d41f3
neckutrek <>
2022-05-29 00:42:22 +0200

    c++ websocket ping pong

--- market_connection/src/trading_MarketChannelImpl.cpp
+++ market_connection/src/trading_MarketChannelImpl.cpp
@@ -27,15 +27,22 @@ JNIEXPORT void JNICALL Java_trading_MarketChannelImpl_connect(
     const auto results = resolver.resolve(host, port);
     boost::asio::connect(ws.next_layer(), results.begin(), results.end());
     ws.handshake(host, "/");
-    ws.write(boost::asio::buffer(text));
-    boost::beast::multi_buffer buffer;
-    ws.read(buffer);
-    ws.close(websocket::close_code::normal);
 
-    std::for_each(buffer.data().begin(), buffer.data().end(), [](auto el){
-        std::cout.write(static_cast<const char*>(el.data()), el.size());
-        });
-    std::cout << std::endl;
+    int n = 0;
+    while (n < 500)
+    {
+      text = std::to_string(n);
+      ws.write(boost::asio::buffer(text));
+      boost::beast::multi_buffer buffer;
+      ws.read(buffer);
+      std::for_each(buffer.data().begin(), buffer.data().end(), [](auto el){
+          std::cout.write(static_cast<const char*>(el.data()), el.size());
+          });
+      std::cout << std::endl;
+      n++;
+    }
+
+    ws.close(websocket::close_code::normal);
   }
   catch (std::exception& e)
   {
