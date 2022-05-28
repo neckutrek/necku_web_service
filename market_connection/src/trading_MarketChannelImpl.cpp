cb7f61bad0557f385f4226927b5dd2e7fce913c4
neckutrek <>
2022-05-29 00:23:23 +0200

    c++ boost websockets

--- market_connection/src/trading_MarketChannelImpl.cpp
+++ market_connection/src/trading_MarketChannelImpl.cpp
@@ -3,8 +3,43 @@
 #include <iostream>
 #include <string>
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+using tcp = boost::asio::ip::tcp;
+namespace websocket = boost::beast::websocket;
+
 JNIEXPORT void JNICALL Java_trading_MarketChannelImpl_connect(
     JNIEnv *, jobject)
 {
-    std::cout << "Java_trading_MarketChannelImpl_connect" << std::endl;
+  std::cout << "Java_trading_MarketChannelImpl_connect" << std::endl;
+  std::string host{"echo.websocket.events"};
+  std::string port{"80"};
+  std::string text{""};
+
+  try
+  {
+    boost::asio::io_context ioc;
+    tcp::resolver resolver{ioc};
+    websocket::stream<tcp::socket> ws{ioc};
+    const auto results = resolver.resolve(host, port);
+    boost::asio::connect(ws.next_layer(), results.begin(), results.end());
+    ws.handshake(host, "/");
+    ws.write(boost::asio::buffer(text));
+    boost::beast::multi_buffer buffer;
+    ws.read(buffer);
+    ws.close(websocket::close_code::normal);
+
+    std::for_each(buffer.data().begin(), buffer.data().end(), [](auto el){
+        std::cout.write(static_cast<const char*>(el.data()), el.size());
+        });
+    std::cout << std::endl;
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
 }
+
